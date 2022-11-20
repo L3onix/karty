9725d6a617b953529fd7ef86ddbcf1fe4e56c27d
L3onix Leonardo Facundes <leonardo.fm00@gmail.com>
2022-11-20 15:30:14 -0300

    refatorando código e criando os métodos CRUD

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,28 +8,88 @@ use Illuminate\Support\Facades\View;
 
 class CustomerController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $customer = Customer::all();
         return View::make('customers.index')->with('customers', $customer);
     }
 
-    public function show($id)
-    {
-        return view('customers.profile', [
-            'customer' => Customer::findOrFail($id)
-        ]);
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('customers.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         Customer::create($request->all());
-    
         return redirect('customers')->with('flash_message', 'Customer created!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('customers.show', [
+            'customer' => Customer::findOrFail($id)
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('customers.edit')
+            ->with('customer', Customer::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::find($id);
+        $inputs = $request->all();
+        $customer->update($inputs);
+        return redirect('customers')->with('flash_message', 'Customer updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Customer::destroy($id);
+        return redirect('customers')->with('flash_message', 'Customer deleted!');
     }
 }
