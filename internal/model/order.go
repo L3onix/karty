dce8a6c21b9f3fc0b6025dd13e52429a8561f6b0
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2025-01-03 19:27:17 -0300

    delete all files

--- internal/model/order.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Order struct {
-	ID       int
-	Client   Client
-	Products []Product
-}
--- internal/model/product.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Product struct {
-	ID            int
-	Name          string  `json:"name"`
-	Price         float64 `json:"price"`
-	AmountInStock int     `json:"amount_in_stock"`
-}
